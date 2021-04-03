6dbc7d4acbcd60c4023e4f4af7576569f26b65ca
Moon-Jang <roby8502@naver.com>
2021-04-03 20:24:27 +0900

    first commit

---
+++ src/main/java/com/moonjang/study/mjspringboot/MjSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.moonjang.study.mjspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjSpringBootApplication.class, args);
+	}
+
+}
